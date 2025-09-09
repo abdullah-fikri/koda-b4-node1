3002d2278233790cfc58cdebb12cf89954c24854
abdullah-fikri <abdullahfikri981@gmail.com>
2025-09-09 23:44:51 +0700

    Add files via upload

---
+++ music/lib/move.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
+const baseFolder = 'musik'
+
+function move(musik) {
+  musik.forEach((item) => {
+    const [artis] = item.split(' - ')
+    const file = path.join(baseFolder, artis)
+
+    if (!fs.existsSync(file)) {
+      fs.mkdirSync(file, { recursive: true})
+    }
+  })
+
+  musik.forEach((item)=>{
+    const artisFinal = item.split(' - ')[0]
+      fs.rename(`./musik/${item}`, `./musik/${artisFinal}/${item}`, (err) => {
+        if (err) throw err;
+      })
+  })
+}
+
+module.exports = {
+    move,
+}
+
