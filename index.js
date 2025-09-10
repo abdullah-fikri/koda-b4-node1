ce9dd8c4cd82f1aa2279bf5d8d4e161e0b5b8ff8
abdullah-fikri <abdullahfikri981@gmail.com>
2025-09-10 16:03:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const generate = require('./lib/lagu')
+const move = require('./lib/move')
+
+const musik = [
+    'dewa 19 - pupus.mp3',
+    'dewa 19 - kangen.mp3',
+    'dewa 19 - satu.mp3',
+    'dewa 19 - aku milikmu.mp3',
+    'peterpan - yang terdalam.mp3',
+    'peterpan - semua tentang kita.mp3',
+    'peterpan - separuh aku.mp3',
+    'ghea indiawari - jangan bersedih.mp3',
+    'ghea indiawari - teramini.mp3',
+    'judika - aku yang tersakiti.mp3',
+]
+
+generate.lagug(musik)
+.then(()=>{
+    console.log('berhasil')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+move.move(musik)
+.then(()=>{
+    console.log('berhasil')
+})
+.catch((err)=>{
+    console.log(err)
+})
